58642be2d433ad583f65901ff6ed9898445ff945
Nils Danielsen <nils.danielsen@hiq.se>
2026-03-29 18:11:20 +0200

    replaced landing page

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <main style={{
+      display: "flex",
+      height: "100vh",
+      alignItems: "center",
+      justifyContent: "center",
+      flexDirection: "column",
+      fontFamily: "sans-serif"
+    }}>
+      <h1>Nils & Nora 💍</h1>
+      <p>Vi gifter oss sommaren 2026</p>
+    </main>
+  );
+}
